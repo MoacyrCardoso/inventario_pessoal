a8b2629eab99ba7c75f934856a8e32a42333b73e
Moacyr Cardoso <moacircardoso2012@gmail.com>
2024-06-19 04:33:01 -0300

    Back End , Banco de dados, conexáo, view

---
+++ view.py
@@ -0,0 +1,56 @@
+# Importando sqlite
+import sqlite3 as lite
+
+# Criando conexão
+con = lite.connect('dados.db')
+
+
+# Inserir dados
+def inserir_form(i):
+    with con:
+         cur = con.cursor()
+         query = "INSERT INTO inventorio(nome, local, descricao, marca, data_da_compra, valor_da_compra, serie, imagem) VALUES(?,?,?,?,?,?,?,?)"
+         cur.execute(query, i)
+
+
+# Atualizar dados
+def atualizar_form(i):
+     with con:
+           cur = con.cursor()
+           query = "UPDATE inventorio SET nome=?, local=?, descricao=?, marca=?, data_da_compra=?, valor_da_compra=?, serie=?, imagem=? WHERE id=? "
+           cur.execute(query, i)
+
+
+# Deletar dados
+def deletar_form():
+    with con:
+         cur = con.cursor()
+         query = " DELETE FROM inventorio WHERE id=?"
+         cur.execute(query, i)
+
+
+# Ver dados
+def ver_form(i):
+    ver_dados = []
+    with con:
+          cur = con.cursor()
+          query = " SELECT * FROM inventorio"
+          cur.execute(query)
+
+          rows = cur.fetchall()
+          for row in rows:
+              ver_dados.append(row)
+    return ver_dados
+
+
+# Ver dados
+def ver_item(id):
+    ver_dados_individual = []
+    with con:
+          cur = con.cursor()
+          query = " SELECT * FROM inventorio WHERE id=?"
+          cur.execute(query, id)
+
+          rows = cur.fetchall()
+          for row in rows:
+              ver_dados_individual.append(row)
